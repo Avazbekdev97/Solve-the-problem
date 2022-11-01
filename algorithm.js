5a323dc1f2909c62b6e72f11919ccc24a057da83
Avazbekdev97 <avazbekdev571@gmail.com>
2022-11-01 10:13:54 +0500

    codewaars

--- algorithm.js
+++ algorithm.js
@@ -20,5 +20,31 @@ console.log(twoSum([3, 2, 4], 6)); */
 }
 console.log(isPalindrome(123)) */
 
+// Check same case codewars
 
+/* function sameCase(a, b){
+    a = a.toString()
+    b = b.toString()
+    const alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    const alphabets2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w','x', 'y', 'z'] 
+    
+    if(!alphabets.includes(a) && !alphabets2.includes(a)) return -1
+    else if(!alphabets.includes(b) && !alphabets2.includes(b)) return -1 
+    const letter1 = alphabets.includes(a)
+    const letter2 = alphabets2.includes(a)
+    const letter3 = alphabets.includes(b)
+    const letter4 = alphabets2.includes(b)
+    if(letter1 === letter3 || letter2 === letter4) return 1 
+    return 0
+}
 
+console.log(sameCase('h', 'w')); */
+
+// Gravity Flip CodeWars
+
+function flip (d, a) {
+    if(d === 'R') return a.sort((a, b) => a - b) 
+    else if (d === 'L') return a.sort((a, b) => b - a)
+}
+
+console.log(flip('L', [3, 2, 1, 2]));
