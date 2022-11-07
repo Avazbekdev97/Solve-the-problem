059ebe6447fd2d88ab1da4133dccd30f66d9476e
Avazbekdev97 <avazbekdev571@gmail.com>
2022-11-07 10:50:56 +0500

    problem solves

--- algorithm.js
+++ algorithm.js
@@ -144,3 +144,22 @@ console.log(shorter_reverse_longer("first", "abcde")); */
 }
 
 console.log(reverseWords("ehT kciuq nworb xof spmuj revo eht yzal .god ")); */
+
+// 13) Vowel Count
+/* Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces. */
+
+/* function getCount(str) {
+    let count = 0
+    for(let i = 0; i < str.length; i++) {
+        if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u') {
+            count += 1
+        }
+    }
+    return count
+}
+
+console.log(getCount('abracadabra')); */
